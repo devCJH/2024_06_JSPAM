2c725221cfac43138e5c70d86a96a87203c0f81c
devCJH <http-@naver.com>
2024-06-11 14:58:29 +0900

    DBConnectTest, ArticleListServlet 진행중

---
+++ src/main/java/com/koreaIT/jsp/am/DBConnectTestServlet.java
@@ -0,0 +1,48 @@
+package com.koreaIT.jsp.am;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/dbtest")
+public class DBConnectTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8;");
+		
+		final String URL = "jdbc:mysql://localhost:3306/jsp_am?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+		final String USER = "root";
+		final String PASSWORD = "";
+
+		Connection connection = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			
+			System.out.println("연결 성공!");
+			
+			response.getWriter().append("연결 성공!!!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
