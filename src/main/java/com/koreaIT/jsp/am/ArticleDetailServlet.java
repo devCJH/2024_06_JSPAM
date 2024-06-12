675737aee4167bdb0ab2a6fb827ee4443a720559
devCJH <http-@naver.com>
2024-06-12 13:32:26 +0900

    article detail, main

--- src/main/java/com/koreaIT/jsp/am/ArticleDetailServlet.java
+++ src/main/java/com/koreaIT/jsp/am/ArticleDetailServlet.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
+
+import com.koreaIT.jsp.am.util.DBUtil;
+import com.koreaIT.jsp.am.util.SecSql;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,13 +15,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/dbtest")
-public class DBConnectTestServlet extends HttpServlet {
+@WebServlet("/article/detail")
+public class ArticleDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF-8;");
-		
 		final String URL = "jdbc:mysql://localhost:3306/jsp_am?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 		final String USER = "root";
 		final String PASSWORD = "";
@@ -28,9 +30,17 @@ public class DBConnectTestServlet extends HttpServlet {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
 			
-			System.out.println("연결 성공!");
+			int id = Integer.parseInt(request.getParameter("id"));
 			
-			response.getWriter().append("연결 성공!!!");
+			SecSql sql = new SecSql();
+			sql.append("SELECT * FROM article");
+			sql.append("WHERE id = ?", id);
+			
+			Map<String, Object> articleMap = DBUtil.selectRow(connection, sql);
+			
+			request.setAttribute("articleMap", articleMap);
+			
+			request.getRequestDispatcher("/jsp/article/detail.jsp").forward(request, response);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
--- src/main/webapp/jsp/article/list.jsp
+++ src/main/webapp/jsp/article/list.jsp
@@ -11,47 +11,18 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>게시물 리스트</title>
 </head>
 <body>
 	<div>
-		<h3>a 태그 사용법</h3>
 		<div><a href="<%= request.getContextPath() %>/home/main">메인</a></div>
-		<div><a href="../home/main">메인2</a></div>
 	</div>
 
-	<div>게시물 리스트 v1</div>
-	<ul>
-		<li><%= articleListMap.get(0).get("id") %> | <%= articleListMap.get(0).get("updateDate") %> | <%= articleListMap.get(0).get("title") %></li>
-		<li><%= articleListMap.get(1).get("id") %> | <%= articleListMap.get(1).get("updateDate") %> | <%= articleListMap.get(1).get("title") %></li>
-		<li><%= articleListMap.get(2).get("id") %> | <%= articleListMap.get(2).get("updateDate") %> | <%= articleListMap.get(2).get("title") %></li>
-	</ul>
-	
-	<div>게시물 리스트 v2</div>
-	<ul>
-		<% for (int i = 0; i < articleListMap.size(); i++) { %>
-			<li><%= articleListMap.get(i).get("id") %> | <%= articleListMap.get(i).get("updateDate") %> | <%= articleListMap.get(i).get("title") %></li>
-		<% } %>
-	</ul>
-	
-	<div>게시물 리스트 v3</div>
-	<ul>
-		<% 
-		for (int i = 0; i < articleListMap.size(); i++) {
-			Map<String, Object> articleMap = articleListMap.get(i);
-		%>
-			<li><%= articleMap.get("id") %> | <%= articleMap.get("updateDate") %> | <%= articleMap.get("title") %></li>
-		<%
-		}
-		%>
-	</ul>
-	
-	<div>게시물 리스트 v4</div>
 	<ul>
 		<% 
 		for (Map<String, Object> articleMap : articleListMap) {
 		%>
-			<li><%= articleMap.get("id") %> | <%= articleMap.get("updateDate") %> | <%= articleMap.get("title") %></li>
+			<li><%= articleMap.get("id") %> | <%= articleMap.get("updateDate") %> | <a href="detail?id=<%= articleMap.get("id") %>"><%= articleMap.get("title") %></a></li>
 		<%
 		}
 		%>
