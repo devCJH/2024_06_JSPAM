4818ada7fc637ab3e12408fc7c8e02c7ec5dd123
devCJH <http-@naver.com>
2024-06-14 14:51:43 +0900

    member join 진행중

---
+++ src/main/java/com/koreaIT/jsp/am/MemberDoJoinServlet.java
@@ -0,0 +1,78 @@
+package com.koreaIT.jsp.am;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.koreaIT.jsp.am.config.Config;
+import com.koreaIT.jsp.am.util.DBUtil;
+import com.koreaIT.jsp.am.util.SecSql;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/doJoin")
+public class MemberDoJoinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html; charset=UTF-8;");
+		
+		Connection connection = null;
+
+		try {
+			Class.forName(Config.getDBDriverName());
+			connection = DriverManager.getConnection(Config.getDBUrl(), Config.getDBUsr(), Config.getDBPW());
+			
+			String loginId = request.getParameter("loginId");
+			String loginPw = request.getParameter("loginPw");
+			String loginPwChk = request.getParameter("loginPwChk");
+			String name = request.getParameter("name");
+			
+			SecSql sql = new SecSql();
+			sql.append("SELECT COUNT(id) FROM `member`");
+			sql.append("WHERE loginId = ?", loginId);
+
+			int loginIdDupChk = DBUtil.selectRowIntValue(connection, sql);
+			
+			if (loginIdDupChk == 1) {
+				response.getWriter().append(String.format("<script>alert('[ %s ]은(는) 이미 사용중인 아이디입니다'); history.back();</script>", loginId));
+				return;
+			}
+			
+			sql = new SecSql();
+			sql.append("INSERT INTO `member`");
+			sql.append("SET regDate = NOW()");
+			sql.append(", updateDate = NOW()");
+			sql.append(", loginId = ?", loginId);
+			sql.append(", loginPw = ?", loginPw);
+			sql.append(", `name` = ?", name);
+			
+			DBUtil.insert(connection, sql);
+			
+			response.getWriter().append(String.format("<script>alert('%s님이 가입되었습니다'); location.replace('../home/main');</script>", loginId));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
